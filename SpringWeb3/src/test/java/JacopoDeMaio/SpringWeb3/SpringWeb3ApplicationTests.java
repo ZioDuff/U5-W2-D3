c3c579f35446f328de04624d49a7f0f5ed90acaa
Jacopo De maio <jajodemaio@gmail.com>
2024-06-26 14:40:52 +0200

    Initial commit

---
+++ SpringWeb3/src/test/java/JacopoDeMaio/SpringWeb3/SpringWeb3ApplicationTests.java
@@ -0,0 +1,13 @@
+package JacopoDeMaio.SpringWeb3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWeb3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
